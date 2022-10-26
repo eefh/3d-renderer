41842ebfbf4ba7c6dfdb164f73e5501d418670f8
Ethan Hasbrouck <ethanhasbrouck02@gmail.com>
2022-10-26 14:53:23 -0400

    clear screen function

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 import drawLine from "./modules/drawLine.js";
 import drawPixel from "./modules/drawPixel.js";
 import drawTri from "./modules/drawTri.js";
-
+import clear from "./modules/clear.js";
 let c = document.getElementById("canvas");
 let ctx = c.getContext("2d");
+let x = 0;
 
-drawTri(ctx, [10, 10], [500, 500], [40, 300], [255, 0, 0, 255]);
+clear(ctx);
+const update = () => {
+    draw();
+    x += 1;
+};
+
+const draw = () => {
+    clear(ctx);
+    drawTri(ctx, [x, 200], [200, 200], [400, 100], [255, 0, 0, 255]);
+};
+
+window.onload = function () {
+    setInterval(update, 20);
+};
---
+++ modules/clear.js
@@ -0,0 +1,5 @@
+const clear = (ctx) => {
+    ctx.clearRect(0, 0, 800, 800);
+};
+
+export default clear;
